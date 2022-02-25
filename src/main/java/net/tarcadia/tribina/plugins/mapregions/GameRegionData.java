8b4b7e260982a771b576c83203e24524c005eeba
Tarcadia <tarcadia@qq.com>
2022-02-26 00:56:55 +0800

    implements BaseRegionData

---
+++ src/main/java/net/tarcadia/tribina/plugins/mapregions/GameRegionData.java
@@ -0,0 +1,18 @@
+package net.tarcadia.tribina.plugins.mapregions;
+
+import net.tarcadia.tribina.plugins.mapregions.base.BaseRegionData;
+import org.bukkit.configuration.ConfigurationSection;
+
+public class GameRegionData implements BaseRegionData {
+
+    @Override
+    public void setConfig(ConfigurationSection config) {
+
+    }
+
+    @Override
+    public ConfigurationSection getConfig() {
+        return null;
+    }
+
+}
