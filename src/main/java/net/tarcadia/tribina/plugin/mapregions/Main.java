99612872854c1b3704003de109d1d0615be1d868
Tarcadia <tarcadia@qq.com>
2022-02-27 05:06:43 +0800

    add static parameters to access these things

--- src/main/java/net/tarcadia/tribina/plugin/mapregions/Main.java
+++ src/main/java/net/tarcadia/tribina/plugin/mapregions/Main.java
@@ -1,8 +1,27 @@
 package net.tarcadia.tribina.plugin.mapregions;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Map;
+import java.util.logging.Logger;
+
 public final class Main extends JavaPlugin {
+
+	public static JavaPlugin plugin;
+	public static FileConfiguration config;
+	public static PluginDescriptionFile descrp;
+	public static Logger logger;
+
+	@Override
+	public void onLoad() {
+		Main.plugin = this;
+		Main.config = this.getConfig();
+		Main.descrp = this.getDescription();
+		Main.logger = this.getLogger();
+	}
+
 	@Override
 	public void onEnable() {
 		// Plugin startup logic
