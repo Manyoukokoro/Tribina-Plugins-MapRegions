98ca538b523407d80e45bdca036d1cef08dd09e2
Tarcadia <tarcadia@qq.com>
2022-02-27 02:30:50 +0800

    new methods added to load and save information, and exceptions managed

--- src/main/java/net/tarcadia/tribina/plugins/mapregions/RegionMap.java
+++ src/main/java/net/tarcadia/tribina/plugins/mapregions/RegionMap.java
@@ -31,18 +31,7 @@ public class RegionMap {
 	public RegionMap(@NonNull String pathConfig, @NonNull String pathMaps) {
 		this.pathConfig = pathConfig;
 		this.pathMaps = pathMaps;
-		try {
-			this.fileConfig = new File(this.pathConfig);
-			this.config = YamlConfiguration.loadConfiguration(this.fileConfig);
-			this.world = UUID.fromString(this.config.getString("world", ""));
-			this.x_offset = this.config.getInt("x_offset");
-			this.z_offset = this.config.getInt("z_offset");
-			this.x_length = this.config.getInt("x_length");
-			this.z_length = this.config.getInt("z_length");
-		} catch (Exception e) {
-			throw new IllegalArgumentException("Illegal configuration arguments", e);
-		}
-
+		this.fileConfig = new File(this.pathConfig);
 		this.regionMap = new TreeMap<>(new Comparator<>() {
 			@Override
 			public int compare(Pair<Integer, Integer> pos1, Pair<Integer, Integer> pos2) {
@@ -53,33 +42,115 @@ public class RegionMap {
 				return pos1.y().compareTo(pos2.y());
 			}
 		});
-
-		ConfigurationSection configRegions = this.config.getConfigurationSection("regions");
-		if (configRegions != null) {
-			this.configRegions = configRegions;
-			for (String regionId : this.configRegions.getKeys(false)) {
-				if (configRegions.getConfigurationSection(regionId) == null) { configRegions.createSection(regionId); }
-				Set<Pair<Integer, Integer>> posSet = new HashSet<>();
-				// TODO: load posSet from a Bitmap file
-				for (Pair<Integer, Integer> pos : posSet) {
-					this.regionMap.put(pos, regionId);
-				}
-			}
-		} else {
-			this.configRegions = this.config.createSection("regions");
+		try {
+			this.load();
+		} catch (IOException e)
+		{
+			// TODO: Log error
 		}
 	}
 
-	public boolean save() throws IOException {
-		this.config.set("world", this.world);
-		this.config.set("x_offset", this.x_offset);
-		this.config.set("z_offset", this.z_offset);
-		this.config.set("x_length", this.x_length);
-		this.config.set("z_length", this.z_length);
+	public void load() throws IOException {
+		this.loadConfigs();
+		this.loadAllMaps();
+	}
+
+	public void loadConfigs() throws IOException {
+		this.config = YamlConfiguration.loadConfiguration(this.fileConfig);
+		this.configRegions = Objects.requireNonNullElseGet(
+				this.config.getConfigurationSection("regions"),
+				() -> this.config.createSection("regions")
+		);
+		try {
+			this.world = UUID.fromString(this.config.getString("world", ""));
+			this.x_offset = this.config.getInt("x_offset");
+			this.z_offset = this.config.getInt("z_offset");
+			this.x_length = this.config.getInt("x_length");
+			this.z_length = this.config.getInt("z_length");
+		} catch (Exception e) {
+			this.world = null;
+			this.x_offset = 0;
+			this.x_length = 0;
+			this.z_offset = 0;
+			this.z_length = 0;
+			throw new IOException("Load config file failed.", e);
+		}
+	}
+
+	public void loadMap(@NonNull String regionId) throws IOException {
+		try {
+			File fileMap = new File(this.pathMaps + "/" + regionId + ".bmp");
+			BufferedImage image = ImageIO.read(fileMap);
+			// TODO: load from a Bitmap file to this.regionMap
+		} catch (Exception e) {
+			throw new IOException("Load map file failed.", e);
+		}
+	}
+
+	public void loadAllMaps() throws IOException {
+		List<Exception> es = new LinkedList<>();
+		for (String regionId : this.configRegions.getKeys(false)) {
+			try {
+				this.loadMap(regionId);
+			} catch (Exception e) {
+				es.add(e);
+			}
+		}
+		if (!es.isEmpty()) {
+			throw new IOException("Load map files failed.", es.get(0));
+		}
+	}
+
+	public void save() throws IOException {
+		this.saveConfigs();
+		this.saveAllMaps();
+	}
+
+	public void saveConfigs() throws IOException {
+		try {
+			this.config.set("world", this.world);
+			this.config.set("x_offset", this.x_offset);
+			this.config.set("z_offset", this.z_offset);
+			this.config.set("x_length", this.x_length);
+			this.config.set("z_length", this.z_length);
+			this.config.save(this.fileConfig);
+		} catch (Exception e) {
+			throw new IOException("Save config file failed.", e);
+		}
+	}
+
+	public void saveMap(@NonNull String regionId) throws IOException {
+		Set<Pair<Integer, Integer>> posSet = new TreeSet<>(new Comparator<>() {
+			@Override
+			public int compare(Pair<Integer, Integer> pos1, Pair<Integer, Integer> pos2) {
+				int result = pos1.x().compareTo(pos2.x());
+				if (result != 0) {
+					return result;
+				}
+				return pos1.y().compareTo(pos2.y());
+			}
+		});
+
+		for (var pos : this.regionMap.keySet()) {
+			if (regionId.equals(regionMap.get(pos))) {
+				// TODO: this can be polished as there should not be always map.get(pos) when traversal it
+				posSet.add(pos);
+			}
+		}
+		try {
+			// TODO: save posSet into a Bitmap file
+		} catch (Exception e) {
+			throw new IOException("Save map file failed.", e);
+		}
+	}
+
+	public void saveAllMaps() throws IOException {
+		List<Exception> es = new LinkedList<>();
 
 		Map<String, Set<Pair<Integer, Integer>>> posSets = new HashMap<>();
 		for (var pos : this.regionMap.keySet()) {
 			var regionId = this.regionMap.get(pos);
+			// TODO: this can be polished as there should not be always map.get(pos) when traversal it
 			var posSet = posSets.computeIfAbsent(regionId, k -> new TreeSet<>(new Comparator<>() {
 				@Override
 				public int compare(Pair<Integer, Integer> pos1, Pair<Integer, Integer> pos2) {
@@ -92,15 +163,17 @@ public class RegionMap {
 			}));
 			posSet.add(pos);
 		}
-		try {
-			this.config.save(this.fileConfig);
-			for (String regionId : this.configRegions.getKeys(false)) {
-				var posSet = posSets.get(regionId);
+
+		for (String regionId : this.configRegions.getKeys(false)) {
+			var posSet = posSets.get(regionId);
+			try {
 				// TODO: save posSet into a Bitmap file
+			} catch (Exception e) {
+				es.add(e);
 			}
-			return true;
-		} catch (Exception e) {
-			throw new IOException("Save file failed", e);
+		}
+		if (!es.isEmpty()) {
+			throw new IOException("Save map files failed.", es.get(1));
 		}
 	}
 
